89044e46e24f13d148f6bd63f88ff21b85475af3
DaKu <do.holyada@gmail.com>
2022-06-24 00:45:57 +0300

    Taskuuu

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -7,3 +7,24 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+
+
+    int[] MyArray = new int[10];
+
+for (int i = 0; i < MyArray.Length; i++)
+{
+    MyArray[i] = new Random().Next(0, 20);
+    Console.Write(MyArray[i] + " ");  
+}
+Console.WriteLine();
+
+int sum =0;
+for (int i = 0; i < MyArray.Length; i++)
+{
+    if (i % 2 !=0) 
+    {
+        sum += MyArray[i];
+    }
+}
+Console.WriteLine("odd position numbers sum: {0}", sum );
+
